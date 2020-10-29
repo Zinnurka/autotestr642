ee640d16b5b947af63d6f8855792c7143fd5d35d
Zinnurka <32505382+Zinnurka@users.noreply.github.com>
2020-10-29 12:29:07 +0500

    Добавлены настройки и драйвер Chrome

---
+++ setting.py
@@ -0,0 +1,9 @@
+import os.path
+
+
+url_admin = 'http://10.0.36.121:83/admin/login/'
+username = ''
+password = ''
+conf_path = os.path.abspath(os.curdir)+'\driver\chromedriver.exe'
+
+print(conf_path)
